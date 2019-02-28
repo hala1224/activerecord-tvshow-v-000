941c3b42ebbd719b52478a62ace4295274580f6b
Natalie Halawani <hala1224@gmail.com>
2019-02-28 20:50:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,10 +1,5 @@
 class AddSeasonToShows < ActiveRecord::Migration
     def change
-        create_table :shows do |t|
-            t.string :name
-            t.string :day
-            t.string :network
-            t.integer :rating
-        end
-    end     
+        add_column :shows, :season, :string
+    end
 end
