190f400781f9938a8aed9908bf67e7e475e12be9
Natalie Halawani <hala1224@gmail.com>
2019-02-28 20:49:20 +0000

    Automatically backed up by Learn

---
+++ db/migrate/002_add_season_to_shows.rb
@@ -0,0 +1,10 @@
+class AddSeasonToShows < ActiveRecord::Migration
+    def change
+        create_table :shows do |t|
+            t.string :name
+            t.string :day
+            t.string :network
+            t.integer :rating
+        end
+    end     
+end
