81d2fdeff4a6e27763eb133f488549859cc7a615
Natalie Halawani <hala1224@gmail.com>
2019-02-28 21:19:47 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,6 +16,16 @@ class Show < ActiveRecord::Base
         self.find_by rating: minimum("rating")
     end
   
+    def self.ratings_sum
+          self.sum("rating")
+      end
   
+      def self.popular_shows
+          self.where("rating > 5")
+      end
   
+      def self.shows_by_alphabetical_order
+          self.order(:name)
+      end
+    
 end
