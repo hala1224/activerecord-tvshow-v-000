2c46b6f5a2e0836602bed16845d69be0aa890042
Natalie Halawani <hala1224@gmail.com>
2019-02-28 21:09:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,19 @@
 class Show < ActiveRecord::Base 
-  def highest_rating
-    Show.maximum(:rating)
-  end
   
+  def self.highest_rating
+        self.maximum("rating")
+    end
+
+    def self.most_popular_show
+        self.find_by rating: maximum("rating")
+    end
+
+    def self.lowest_rating
+        self.minimum("rating")
+    end
+
+    def self.least_popular_show
+        self.find_by rating: minimum("rating")
+    end
   
 end
